ddf9ce1240b689b2c180aeb674d913a00964e5e5
aMalhotra19 <anjumalhotra1989@gmail.com>
2022-11-05 19:29:51 -0700

    Added basic project

---
+++ NasaApp/NasaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAppApp.swift
+//  NasaApp
+//
+//  Created by Anju Malhotra on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct NasaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
